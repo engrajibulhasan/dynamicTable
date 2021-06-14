db05fddfa33940449cbefe008674083c50f0c335
appsmakerbd <engrajibulhasan@gmail.com>
2021-06-14 21:41:08 +0600

    almost finish

--- src/component/Row/Row.js
+++ src/component/Row/Row.js
@@ -1,27 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 const Row = (props) => {
   const { id, col1, col2, col3, col4, status } = props.info;
-  
-  
 
-    return (
-      <tr>
-        <td>{id}</td>
-        <td>{col1}</td>
-        <td>{col2}</td>
-        <td>{col3}</td>
-        <td>
-          <input type="text" placeholder="User Name" />
-        </td>
-        <td>
-          <input type="text" placeholder="Dependent Data" />
-        </td>
-        <td>
-          <button onClick={() => props.handleData(id)}>Send</button>
-        </td>
-      </tr>
-    );
+  const [userName, setUserName] = useState("");
+  const [dependant, setDependant] = useState("");
+  const handleUserName = (payload) => {
+    setUserName(payload);
+  };
+
+  useEffect(() => {
+    setDependant(props.parentData);
+  }, [props.parentData]);
+
+  const handleDependant = (payload) => {
+    setDependant(payload);
+  };
+
+  return (
+    <tr>
+      <td>{id}</td>
+      <td>{col1}</td>
+      <td>{col2}</td>
+      <td>{col3}</td>
+      <td>
+        <input
+          onChange={(e) => handleUserName(e.target.value)}
+          name="userName"
+          type="text"
+          placeholder="User Name"
+        />
+      </td>
+      <td>
+        <input
+          onChange={(e) => handleDependant(e.target.value)}
+          name="dependant"
+          type="text"
+          placeholder="Dependent Data"
+          value={dependant}
+        />
+      </td>
+      <td>
+        <button
+          onClick={() =>
+            props.handleData({
+              id,
+              userName,
+              dependant,
+            })
+          }
+        >
+          Send
+        </button>
+      </td>
+    </tr>
+  );
 };
 
 export default Row;
--- src/component/Table/Table.js
+++ src/component/Table/Table.js
@@ -1,77 +1,82 @@
-import React, { useState } from 'react';
-import Row from '../Row/Row';
-import './Table.css';
-
+import React, { useState } from "react";
+import Row from "../Row/Row";
+import "./Table.css";
 
 const Table = () => {
-    const fakeData = [
-      {
-        id: 1,
-        col1: "Data 1",
-        col2: "Data 2",
-        col3: "Data 3",
-        col4: "",
-        status: 0,
-      },
-      {
-        id: 2,
-        col1: "Data 1",
-        col2: "Data 2",
-        col3: "Data 3",
-        col4: "",
-        status: 0,
-      },
-      
-      {
-        id: 3,
-        col1: "Data 1",
-        col2: "Data 2",
-        col3: "Data 3",
-        col4: "",
-        status: 1,
-      },
-    ];
+  const fakeData = [
+    {
+      id: 1,
+      col1: "Data 1",
+      col2: "Data 2",
+      col3: "Data 3",
+      col4: "",
+      status: 0,
+    },
+    {
+      id: 2,
+      col1: "Data 1",
+      col2: "Data 2",
+      col3: "Data 3",
+      col4: "",
+      status: 0,
+    },
 
-    const handleData = (id) => {
-      console.log(id);
-    }
+    {
+      id: 3,
+      col1: "Data 1",
+      col2: "Data 2",
+      col3: "Data 3",
+      col4: "",
+      status: 1,
+    },
+  ];
 
-    const [parentData, setParentData] = useState('');
-    console.log("the data is", parentData);
+  const handleData = ({ id, userName, dependant }) => {
+    console.log(id);
+    console.log(userName);
+    console.log(dependant);
+  };
 
+  const [parentData, setParentData] = useState("");
+  console.log(parentData);
 
-    return (
-      <div>
-        <label htmlFor="parentData">Parent Data</label>
-        <input
-          type="text"
-          name="parentData"
-          id="parentData"
-          onKeyUp={() => setParentData()}
-        />
-        <br />
-        <br />
+  return (
+    <div>
+      <label htmlFor="parentData">Parent Data</label>
+      <input
+        type="text"
+        name="parentData"
+        id="parentData"
+        onKeyUp={(e) => setParentData(e.target.value)}
+      />
+      <br />
+      <br />
 
-        <table className="dynamic-table">
-          <thead>
-            <tr>
-              <th>SL</th>
-              <th>Col1</th>
-              <th>Col2</th>
-              <th>Col3</th>
-              <th>Col4 Custom</th>
-              <th>Col5 Custom</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {fakeData.map((dt) => (
-              <Row key={dt.id} info={dt} handleData={handleData} />
-            ))}
-          </tbody>
-        </table>
-      </div>
-    );
+      <table className="dynamic-table">
+        <thead>
+          <tr>
+            <th>SL</th>
+            <th>Col1</th>
+            <th>Col2</th>
+            <th>Col3</th>
+            <th>Col4 Custom</th>
+            <th>Col5 Custom</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {fakeData.map((dt) => (
+            <Row
+              key={dt.id}
+              info={dt}
+              parentData={parentData}
+              handleData={handleData}
+            />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Table;
